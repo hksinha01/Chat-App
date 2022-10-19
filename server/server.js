4cc3af72cd5923d4b687d04360a039cd61927d0a
hksinha01 <hksinha19012001@gmail.com>
2022-10-20 02:04:31 +0530

    connecting socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require("path")
+const http = require("http")
 const express = require("express")
+const socketIO = require("socket.io")
 
 const publicPath = path.join(__dirname,"/../public")
-var app = express()
+let app = express()
 const port = process.env.PORT|| 3000
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
+io.on("connection",(socket)=>{
+    console.log("A new user just connected")
+
+    socket.on('disconnect', ()=>{
+        console.log("User was disconnected")
+    })
+})
+
+
+
+server.listen(port, ()=>{
     console.log(`Server id up on port ${port}`)
 })
